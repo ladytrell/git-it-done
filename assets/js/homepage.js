a6b37a31c2f369f8da91c171d0ec0c152171d152
Lady Trell <antrellkent@gmail.com>
2022-03-13 01:08:57 -0500

    final CSS and error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,36 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
-  
-  getUserRepos();
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+};
+
+var getUserRepos = function(user) {
+    // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+    } else {
+    alert("Please enter a GitHub username");
+    }
+};
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
